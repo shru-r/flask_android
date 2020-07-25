6add052feed1405504ae5ddbf514e7538da16f25
shru-r <shrutirampure412@gmail.com>
2020-07-25 13:56:58 +0530

    flask done

--- flask_app/AndroidFlask/app.py
+++ flask_app/AndroidFlask/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-    d = {'username':'Shruti', 'password':'12345', 'remember me':'true'}
+    d = {'username':'s', 'password':'12345', 'remember me':'true'}
     return jsonify(d)
 
 # app.run(host= '0.0.0.0')
